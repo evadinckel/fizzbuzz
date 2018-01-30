227efcafb570a6ab47663dc8c8de6d02d1cb344e
Eva.Dinckel <evadinckel@gmail.com>
2018-01-30 17:16:00 +0000

    fizzbuzz exercise with mentor during pre-course. Intro to RSpec

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,19 @@
+
+def fizzbuzz(number)
+  puts 'fizz' if number % 3 == 0
+  puts 'buzz' if number % 5 == 0
+  # puts 'fizzbuzz' if number % 3 == 0 && number % 5 == 0
+end
+
+
+# if a>10000 && b == "M"
+#   puts "Your salary is good and you are a male"
+# elsif a>10000 && b == "F"
+#   puts "Your salary is good and you are a female"
+# elsif a<=10000 && b == "M"
+#   puts "Your salary is not so good and you are a male"
+# elsif a<=10000 && b == "F"
+#   puts "Your salary is not so good and you are a female"
+# else
+#   puts "Enter valid input"
+# end
